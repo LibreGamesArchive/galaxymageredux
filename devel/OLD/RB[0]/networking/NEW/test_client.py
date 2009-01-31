bd70c59f93cee45228d407cdc0ac97c900c0b997
archwyrm <archwyrm@a3fbd688-2d3d-0410-b8ac-e166fe2dbcef>
2009-01-31 22:30:53 +0000

    Properly threaded taking input.

--- devel/OLD/RB[0]/networking/NEW/test_client.py
+++ devel/OLD/RB[0]/networking/NEW/test_client.py
@@ -1,12 +1,28 @@
 import newnet
+from twisted.internet import reactor, threads
 
 class TestClient(newnet.Client):
-    def update(self):
-        line = raw_input(":->")
+    def __init__(self, host, port, username):
+        newnet.Client.__init__(self, host, port, username)
+        self.get_input = True
+
+    def input_received(self, line):
+        """Callback for to send out a finished line of input."""
         if line == "q":
             c.close()
-        else:
+        elif len(line) > 0:
             c.avatar.callRemote("sendMessage", line)
+        self.get_input = True
+
+    def update(self):
+        # Only run this code once at a time!
+        if self.get_input:
+            # Thread this blocking call, which immediately returns a deferred
+            d = threads.deferToThread(raw_input, ":->")
+
+            # Execute function as soon as input is available
+            d.addCallback(self.input_received)
+            self.get_input = False
 
 c = TestClient("localhost", 44444, "test!")
 c.connect()
