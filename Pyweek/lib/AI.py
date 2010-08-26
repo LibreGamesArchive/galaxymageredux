fce4d28b5fd6c81bc19f1c3b667b9d0ca5d07b77
durandal <durandal@a3fbd688-2d3d-0410-b8ac-e166fe2dbcef>
2010-08-26 01:17:03 +0000

    Removed network code from AI class. "Cleaned up" code a little.

--- Pyweek/lib/AI.py
+++ Pyweek/lib/AI.py
@@ -1,65 +1,58 @@
-""" Artificial Intelligence, implemented as a SLG.Client
-    Initialize with the master of the game, the game itself and the scenario
-    After initialized acts similarly to a player.
+""" Artificial Intelligence
+    Really just a static class with some public decision-making methods.
+    1. Could be overloaded in the future to give decisions attributes like:
+       aggression or survivalism.
     Robert Ramsay
 """
 
-from time import time
-from lib import SLG, event, client_game_engine
 from math import sqrt
 
-class AI_Client(SLG.Client):
-    def __init__(self, master, game, scenario, name = None):
-        self.game = game
-        self.scen = scenario
-        if name == None or len(name) == 0:
-            # Get a pseudo unique name in the case of 1+ bots and 0 creativity
-            t = time()
-            name = 'Robot' + str(int(t*1000) - int(t/10)*10000)
-        SLG.Client.__init__(self, name, master.hostname, master.port)
-        self.Connect()
-
-    def update(self):
-        if my_turn:
-            for unit in my_team:
-                closest_enemy = find_closest_enemy(unit, game)
-                if unit.type == fighter:
-                    unit.move(closest_enemy)
-                    unit.attack(closest_enemy)
-                if unit.type == archer:
+class AI_Player:
+    def update(self,
+               gamestate, # Current status of game: map, units, locations
+               gameactions, # The class that has the methods the AI must call
+               my_team # Sequence of units that make up this time. Might be 
+                       #  good enough to just give the team name and get the
+                       #  units from teh gamestate.
+               ):
+        for unit in my_team:
+            closest_enemy = find_closest_enemy(unit, gamestate.map)
+            if unit.type == fighter:
+                unit.move(closest_enemy)
+                unit.attack(closest_enemy)
+            if unit.type == archer:
+                unit.move_range_limit(closest_enemy)
+                unit.attack(closest_enemy)
+            if unit.type == mage:
+                location = find_closest_injured(unit, 0.5, game)
+                if location:
+                    unit.move_range_limit(location)
+                    unit.heal(location)
+                else:
                     unit.move_range_limit(closest_enemy)
                     unit.attack(closest_enemy)
-                if unit.type == mage:
-                    location = find_closest_injured(unit, 0.5, game)
-                    if location:
-                        unit.move_range_limit(location)
-                        unit.heal(location)
-                    else:
-                        unit.move_range_limit(closest_enemy)
-                        unit.attack(closest_enemy)
 
-    def find_closest_enemy(unit, game):
-        start = (unit.X, unit.Y)
+    def find_closest_enemy(unit, map):
+        x = unit.X
+        y = unit.Y
         r = 1
         enemy = None
-        map = game.map
         while not enemy and r < unit.range + unit.mv:
             potentials = list()
-            for i in range(start.X-r,start.X+r):
-                for j in range(start.Y-r,start.Y+r):
-                    if map[i,j] is unit and
-                       my_team.index(map[i,j]) == None and
-                       map[i,j].hp > 0:
-                        potentials.append(map[i,j])
+            for i in range(x-r,x+r):
+                for j in range(y-r, y+r):
+                    if map[i][j] is unit and my_team.index(map[i][j]) == None and map[i][j].hp > 0:
+                        potentials.append(map[i][j])
             for e in potentials:
                 if enemy:
                     if enemy.hp > e.hp: 
                         enemy = e
                 else:
                     enemy = e
+            r += 1
         return enemy
 
-    def find_closest_injured(unit, threshold, game):
+    def find_closest_injured(unit, game, threshold = 0.5,):
         r_max = unit.mv + unit.range
         r = r_max + 1
         patient = None
