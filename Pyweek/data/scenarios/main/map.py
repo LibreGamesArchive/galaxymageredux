bc5a8ca94be79660418ca76e9fb38cb3e3dc43fd
lord.don <lord.don@a3fbd688-2d3d-0410-b8ac-e166fe2dbcef>
2010-08-27 05:02:29 +0000

    Switched unit graphics to red fighter.

--- Pyweek/data/scenarios/main/map.py
+++ Pyweek/data/scenarios/main/map.py
@@ -10,10 +10,10 @@ mapd.map_grid = [[1,1,1,1,1],
 mapd.engine.camera.set_pos(2.5, 1.5)
 
 #no name is needed, but Bob is there so I can identify entity later!
-mapd.make_entity('test-ent1.gif', (0.5,0.5), 'Bob')
-mapd.make_entity('test-ent1.gif', (1.5,0.5), 'Bob')
-mapd.make_entity('test-ent1.gif', (0.5,1.5), 'Bob')
-mapd.make_entity('test-ent1.gif', (1.5,1.5), 'Bob')
+mapd.make_entity('unit-test-fighter.gif', (0.5,0.5), 'Bob')
+mapd.make_entity('unit-test-fighter.gif', (1.5,0.5), 'Bob')
+mapd.make_entity('unit-test-fighter.gif', (0.5,1.5), 'Bob')
+mapd.make_entity('unit-test-fighter.gif', (1.5,1.5), 'Bob')
 
 for i in mapd.get_entities_on_tile(2,1):
     print [i.name]
