c71200e3c038408a925449d49736bf98f4f94dfd
RoeBros <RoeBros@a3fbd688-2d3d-0410-b8ac-e166fe2dbcef>
2010-08-28 22:06:19 +0000

    Even better ai!

--- Pyweek/data/scenarios/CaughtByTheEnemy/ai.py
+++ Pyweek/data/scenarios/CaughtByTheEnemy/ai.py
@@ -31,7 +31,7 @@ class AI(BaseAI):
                     if a.name == 'Move':
                         #if enemy_list and len(enemy_list:
                         bt,pt = a._get_blocked_tiles()
-                        p = a.get_path(u.pos, enemy_list[0].pos, bt)
+                        p = a.get_path(u.pos, enemy_list[0].pos, bt+pt)
                         # Just move once, because there may be a closer enemy or
                         # an enemy in range if we move.
 
