497b2dbb3709eceed99774db3841b2bb1261b568
durandal <durandal@a3fbd688-2d3d-0410-b8ac-e166fe2dbcef>
2010-08-30 01:41:52 +0000

    Refactored AI code.

--- Pyweek/data/scenarios/CaughtByTheEnemy/ai.py
+++ Pyweek/data/scenarios/CaughtByTheEnemy/ai.py
@@ -3,103 +3,44 @@ class AI(BaseAI):
     def initialize(self):
         pass
 
-    def goodguys_update(self):
-        enemy_list = self.get_enemy_units()
-        team_list = self.get_my_units()
-        if not (enemy_list or team_list):
-            self.end_my_turn()
-        
-        for u in team_list:
-            if u.dead:
-                continue
+    def get_target(self, unit, enemy_list):
+        ''' 'goodguys' target jail cell (14,16), then target map exit (0,0)
+            'badguys' target closest 'goodguy', and ignore the prisoner until
+            he is free.
+        '''
+        goal = None
+        if self.team == 'goodguys':
             if self.scenario.mod.prisoner_free:
                 goal = (0,0)
             else:
                 goal = (16,14)
-            if u.type == 'prisoner':
-                if self.scenario.mod.prisoner_free:
-                    for a in u.actions:
-                        if a.name == 'Move':
-                            bt,pt = a._get_blocked_tiles()
-                            if enemy_list:
-                                p = a.get_path(u.pos, goal, bt+pt)
-                            else:
-                                p = None
-                            tar = None
-                            if p:
-                                for i in p:
-                                    if a.test_acceptable(i):
-                                        tar = i
-                                        break
-                            if tar:
-                                self.do_action(u, a, i)
-                            else:
-                                u.cur_ap = 0
-                            break
+        elif self.team == 'badguys':
+            for i in enemy_list:
+                if i.type == 'prisoner' and not self.scenario.mod.prisoner_free:
+                    continue
+                if i.dead:
+                    continue
+                goal = i.pos
                 break
-
-            enemy_list.sort(key=lambda e: self.distance(e.pos,u.pos))
-            # AI always moves to the closest enemy.
-            while u.cur_ap > 0:
-                for a in u.actions:
-                    if a.name == 'Move':
-                        continue # First try an attacking ability
-                    for e in enemy_list:
-                        if a.test_acceptable(e.pos):
-                            self.do_action(u, a, e.pos)
-
-                # Now we make first attempt at moving.
-                if u.cur_ap < 1:
-                    break
-                
-                for a in u.actions:
-                    if a.name == 'Move':
-                        #if enemy_list and len(enemy_list:
-                        bt,pt = a._get_blocked_tiles()
-                        if enemy_list:
-                            p = a.get_path(u.pos,goal, bt+pt)
-                        else:
-                            p = None
-                        tar = None
-                        if p:
-                            for i in p:
-                                if a.test_acceptable(i):
-                                    tar = i
-                                    break
-                        if tar:
-                            self.do_action(u, a, i)
-                        else:
-                            u.cur_ap = 0
-                        break
-
-        self.end_my_turn()   
+        else:
+            print('ai.py: team not in scenario')
+        return goal 
 
     def update(self):
-        if self.team == 'goodguys':
-            self.goodguys_update()
-            return
-        
         enemy_list = self.get_enemy_units()
         team_list = self.get_my_units()
         if not (enemy_list or team_list):
             self.end_my_turn()
-        if not self.scenario.mod.prisoner_free:
-            for i in enemy_list:
-                if i.type == 'prisoner':
-                    enemy_list.remove(i)
-                    break
-        # I think we need an AIHandler
-        # This would allow each "ai" class in the scenarios.ai.py to be customized by team
         for u in team_list:
             if u.dead:
                 continue
             enemy_list.sort(key=lambda e: self.distance(e.pos,u.pos))
             # AI always moves to the closest enemy.
             while u.cur_ap > 0:
+                target = self.get_target(u, enemy_list)
                 if u.name == 'Guard':
-                    if enemy_list:
-                        if self.distance(enemy_list[0].pos, u.pos) > 4:
-                            break
+                    if self.distance(target, u.pos) > 4:
+                        break
                 for a in u.actions:
                     if a.name == 'Move':
                         continue # First try an attacking ability
@@ -111,17 +52,14 @@ class AI(BaseAI):
                 if u.cur_ap < 1:
                     break
                 
+                #Refresh target, incase we killed some one.
+                target = self.get_target(u, enemy_list)
+                if not target:
+                    break
                 for a in u.actions:
                     if a.name == 'Move':
-                        #if enemy_list and len(enemy_list:
                         bt,pt = a._get_blocked_tiles()
-                        if enemy_list:
-                            p = a.get_path(u.pos, enemy_list[0].pos, bt+pt)
-                        else:
-                            p = None
-                        # Just move once, because there may be a closer enemy or
-                        # an enemy in range if we move.
-
+                        p = a.get_path(u.pos, target, bt+pt)
                         tar = None
                         if p:
                             for i in p:
@@ -133,7 +71,6 @@ class AI(BaseAI):
                         else:
                             u.cur_ap = 0
                         break
-
-        self.end_my_turn()   
+        self.end_my_turn() 
 
 store.ai = AI
